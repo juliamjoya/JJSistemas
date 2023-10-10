2618373cd05bfdc9277816c1e43b9f9578189e22
juliamjoya <julian_joya@yahoo.es>
2023-10-09 19:16:25 -0500

    Actualizacion de los archivos de APi y User

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const Routes = require('./routes');
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+app.use('/users', Routes.UserRoutes);
+app.use('/api', Routes.ApiRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const router = express.Router();
+//const controllers = require('../controllers'); //Usando Require
+const {UserControllers} = require('../controllers'); //Usando destructer
+
+//router.get('/', controllers.UserControllers.getUsers);
+router.get('/', UserControllers.getUsers);
+
+router.post('/', UserControllers.createUser);
+
+router.put('/:id', UserControllers.putUser);
+
+router.patch('/:id', UserControllers.updateUser);
+
+router.delete('/:id', UserControllers.deleteUser);
+
+module.exports = router;
