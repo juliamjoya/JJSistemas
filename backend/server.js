13add24c1998a3ead07f6da296f2aed3522b87fd
juliamjoya <julian_joya@yahoo.es>
2023-10-13 17:06:08 -0500

    Actulizando los archivos del backend para hacer el despliegue en render

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const server = express();
+const port = 3000;
+
+const Routes = require('./routes');
+
+server.get('/', (req, res) => {
+  res.send('API para la tienda virtual JJsistemas');
+});
+
+// server.use(express.json()); //Para que parsee lo que recibe en JSON
+// server.use('/users', Routes.UserRoutes);
+// server.use('/api', Routes.ApiRoutes);
+
+server.listen(port, () => {
+  console.log(`API JJSistemas escuchando en el puerto ${port}`);
+});
