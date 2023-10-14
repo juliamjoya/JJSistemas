2f70f7c1ca21ab62cc424ae4888b15393f5aaff9
juliamjoya <julian_joya@yahoo.es>
2023-10-13 22:20:31 -0500

    Se agrego el logo de la empresa al frontend

--- frontend/JJsistemas/src/components/home/Home.jsx
+++ frontend/JJsistemas/src/components/home/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import "./home.css"
+import logoEmpresa from '../../assets/logo_jjsistemas.png'
 
 const Home = () => {
   return (
     <div>
         <div className="jumbotron">
-        <h1 className="display-4">Proximamente, Tienda Virtual JJsistemas</h1>
-        <p className="lead">Cordialmente los invito en unos dias a la inauguracion de la tienda virtual JJsistemas</p>
-        <hr className="my-4" />
-        <p>La tienda virtual JJsistemas ofrecera venta de perifericos/accesorios para computadores y demas tambien ofrecera el servicio de desarrollos de software a la medida del cliente.</p>
+            <img className="logoEmpresa" src={logoEmpresa} alt="Logo JJsistemas" />
+            <h1 className="display-4">Proximamente, Tienda Virtual JJsistemas</h1>
+            <p className="lead">Cordialmente los invito en unos dias a la inauguracion de la tienda virtual JJsistemas</p>
+            <hr className="my-4" />
+            <p>La tienda virtual JJsistemas ofrecera venta de perifericos/accesorios para computadores y demas tambien ofrecera el servicio de desarrollos de software a la medida del cliente.</p>
         </div>
   </div>
   )
